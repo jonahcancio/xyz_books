565201823e2033a4bdbe2f65ac271d7f30c28914
Jonathan Cancio <jonahcancio@gmail.com>
2022-10-24 21:37:03 +0800

    Added basic bootstrap Jumbo

--- frontend/src/App_backup.vue
+++ frontend/src/App_backup.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
-    <Jumbo />
+    <img
+      alt="Vue logo"
+      src="./assets/logo.png"
+    >
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import Jumbo from './components/Jumbo.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    Jumbo
+    HelloWorld
   }
 }
 </script>
---
+++ frontend/src/components/Jumbo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <b-jumbotron
+      header="BootstrapVue"
+      lead="Bootstrap v4 Components for Vue.js 2"
+    >
+      <p>For more information visit website</p>
+      <b-button
+        variant="primary"
+        href="#"
+      >
+        More Info
+      </b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Jumbo",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
